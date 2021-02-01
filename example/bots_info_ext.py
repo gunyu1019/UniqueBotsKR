329d2cebe9f9d26cc1bbe9a5dc8f502c2df308ec
PLRS <67216793+Paloras@users.noreply.github.com>
2021-02-01 23:06:02 +0900

    Update bots_info_ext.py

--- example/bots_info_ext.py
+++ example/bots_info_ext.py
@@ -13,13 +13,15 @@ unitoken='토큰'
 
 @bot.command(name="유니크봇리스트",aliases=["uniquebotslist"], help="로드을 합니다.")
 async def uniquebots_list(ctx):
+    status_dict: dict = {'online': '온라인', 'offline': '오프라인','invisible': '오프라인', 'idle': "자리비움",'dnd': "방해금지"}
     Unique=UniqueBotsKR.client(bot,unitoken)
     data= await Unique.getBots()
     n = 1
     rank=[]
-    embed = discord.Embed(title="봇하트순위", description ='[투표하기](https://uniquebots.kr/bots/info/723346442932191302)')
+    embed = discord.Embed(title="봇하트순위", description ='[홈페이지](https://uniquebots.kr/)')
     for x in data:
-        rank.append([x.nam,x.status,x.guilds,len(x.hearts),x.prefix,x.brief,x.invite])
+        status = status_dict[x.status]
+        rank.append([x.name,status,x.guilds,len(x.hearts),x.prefix,x.brief,x.invite])
     rank = list(reversed(sorted(rank, key=lambda x:x[1])))
     for i in rank:
         if n>11:
@@ -27,5 +29,5 @@ async def uniquebots_list(ctx):
         embed.add_field(name=f'{n}',value=f' {i[0]} {i[1]}\n{i[2]}서버 ❤️ {i[3]} 접두사:{i[4]}\n짧은설명:{i[5]}\n[초대하기]({i[6]})')
         n += 1
     await ctx.send(embed= embed)
-
+    
 bot.run(TOKEN)
