5ea3a0b71ce9e90b0050d9f29f5199f4786e6b68
PLRS <67216793+Paloras@users.noreply.github.com>
2021-02-01 23:14:35 +0900

    Update bots_info_ext.py

--- example/bots_info_ext.py
+++ example/bots_info_ext.py
@@ -14,9 +14,9 @@ unitoken='토큰'
 @bot.event
 async def on_ready():
     print("디스코드 봇 로그인이 완료되었습니다.")
-    print("디스코드봇 이름:" + client.user.name)
-    print("디스코드봇 ID:" + str(client.user.id))
-    print("디스코드봇 버전:" + str(discord.__version__))
+    print("디스코드봇 이름:" + bot.user.name)
+    print("디스코드봇 ID:" + str(bot.user.id))
+    print("디스코드봇 버전:" + str(bot.__version__))
     print('------')
 
 @bot.command(name="유니크봇리스트",aliases=["uniquebotslist"], help="로드을 합니다.")
